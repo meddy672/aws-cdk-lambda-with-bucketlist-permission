ac0d668a7a2f822c6561c03e4ba2a434c7914f21
Matthew Eddy <meddy672@gmail.com>
2022-07-25 09:56:13 -0400

    first commit

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Welcome to your CDK TypeScript project
-
-This is a blank project for CDK development with TypeScript.
+This is a sample project built with the AWS CDK that provisions an AWS Lambda function with S3 list bucket permissions. When invoked, the lambda will log the buckets for the account to the console and return a 200 status code with message *Hello from the lambda*
+![Alt Text](./images/aws-lambda.png)
 
 The `cdk.json` file tells the CDK Toolkit how to execute your app.
 
--- lib/aws-cdk-lambda-stack.ts
+++ lib/aws-cdk-lambda-stack.ts
@@ -1,16 +1,38 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Stack, StackProps, Duration } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as path from 'path';
 
 export class AwsCdkLambdaStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+  
+    // lambda function definition
+    const lambdaFunction = new lambda.Function(this, 'MyLambda', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      memorySize: 1024,
+      timeout: Duration.seconds(5),
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset(path.join(__dirname, '/../src')),
+      environment: {
+        REGION: Stack.of(this).region,
+        AVAILABILITY: JSON.stringify(
+          Stack.of(this).availabilityZones
+        )
+      }
+    });
 
-    // The code that defines your stack goes here
+    const listBucketsPolicy = new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: ['s3:ListAllMyBuckets'],
+      resources: ['arn:aws:s3:::*']
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkLambdaQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    lambdaFunction.role?.attachInlinePolicy(
+      new iam.Policy(this, 'list-buckets', {
+        statements: [listBucketsPolicy]
+      })
+    )
   }
 }
